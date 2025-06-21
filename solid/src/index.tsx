21c848a003b8edf818cc8df7c820ecf85487e157
wiktrek <wiktor.janiszewski10@gmail.com>
2025-06-21 22:22:48 +0200

    feat: created main.go + added logo

--- solid/src/index.tsx
+++ solid/src/index.tsx
@@ -2,6 +2,10 @@
 import { render } from "solid-js/web";
 const root = document.getElementById("root");
 const Home = () => {
-  return <p>Test</p>;
+  return (
+    <>
+      <p>What's this project? - idk</p>
+    </>
+  );
 };
 render(() => <Home />, root!);
