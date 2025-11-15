965b7b3c57598077cc0472206c9ae58ab4044d23
wiktrek <wiktor.janiszewski10@gmail.com>
2025-11-15 15:28:25 +0100

    feat: solidstart

--- solid/src/index.tsx
+++ solid/src/index.tsx
@@ -1,90 +1,15 @@
 /* @refresh reload */
-import "./styles/global.css";
-import { render } from "solid-js/web";
-import { createSignal } from "solid-js/types/server/reactive.js";
-import "../styles/global.css";
-/*
+import { render } from 'solid-js/web';
+import 'solid-devtools';
 
-  Move to solid start!!!!!!!!!
-  
-*/
-const root = document.getElementById("root");
-interface Snake {
-  head: Point;
-  parts?: SnakePart[];
-}
+import App from './App';
 
-interface SnakePart {
-  position: Point;
-}
-interface Point {
-  x: number;
-  y: number;
-}
-const Home = () => {
-  const grid_size = 9;
-  const [points, setPoints] = createSignal(0);
-  const [grid, setGrid] = createSignal([]);
-  const snake = {
-    head: {
-      x: 0,
-      y: 0,
-    },
-  } as Snake;
-  let is_apple = false;
-  const Grid = (props: { size: number }) => {
-    let elements = [];
-    for (let index = 0; index < props.size ** 2; index++) {
-      elements.push(index);
-    }
-    return (
-      <>
-        {elements.map((i) => (
-          <div
-            class="border-2 w-16 h-16"
-            style={{
-              "background-color": "lightgreen",
-            }}
-            id={`${i}`}
-          />
-        ))}
-      </>
-    );
-  };
-  function spawn_snake() {
-    document.getElementById("0")!.style.backgroundColor = "green";
-    if (!is_apple) {
-      spawn_apple();
-    }
-  }
-  function spawn_apple() {
-    let random = Math.random() * grid_size ** 2;
-    let random_element = document.getElementById(`${Math.floor(random)}`)!;
-    while (random_element.style.backgroundColor === "green") {
-      random = Math.random() * grid_size ** 2;
-      random_element = document.getElementById(`${Math.floor(random)}`)!;
-    }
-    random_element.style.backgroundColor = "red";
-    is_apple = true;
-  }
-  function eat_apple() {
-    /* 
-      code here
-    */
+const root = document.getElementById('root');
 
-    setPoints((prev) => prev + 1);
-  }
-  return (
-    <>
-      <div class="flex justify-center text-center items-center text-3xl flex-col bg-slate-800 h-screen text-white">
-        <h1>This is a snake app!</h1>
-        <p>Points: {points()}</p>
-        <button onClick={spawn_snake}>Start</button>
-        <div class="grid grid-cols-9 grid-rows-9 pt-4">
-          <Grid size={grid_size} />
-        </div>
-      </div>
-    </>
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
   );
-};
-render(() => <Home />, root!);
+}
+
+render(() => <App />, root!);
--- solid/vite.config.ts
+++ solid/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
+
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
-  base: "/solid/",
+  plugins: [devtools(), solidPlugin()],
+  server: {
+    port: 3000,
+  },
   build: {
-    outDir: "dist",
+    target: 'esnext',
   },
 });
